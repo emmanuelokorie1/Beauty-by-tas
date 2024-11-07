b3327d6c049fa64707897888016e3331c43fc708
Okorie Emmanuel <okorieemmanuel167@gmail.com>
2024-11-07 16:30:14 +0100

    changed base

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://c92109da85c626a82626328ae52584a0.serveo.net',
+  baseURL: 'https://beautybytas.sytes.net',
+  // baseURL: 'https://c92109da85c626a82626328ae52584a0.serveo.net',
   // baseURL: 'http://95.169.205.185:3200',
 });
 
